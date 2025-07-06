365b3255d8e3ad5d3462cb6c429222f7a3fcabf9
Varunkohli018 <varunkohli18h@gmail.com>
2025-07-06 23:16:01 +0530

    Update script.js

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ async function getBotReply(message) {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
-        "Authorization": "Bearer YOUR_API_KEY"
+        "Authorization": sk-proj-wcIqBozFCPi7FClE3YbShy5Uf-eQIAOqsi19_6tmXwY1lgJzd8r-4DqY6NSXatSd2Ets2cpJ0VT3BlbkFJ8r__1WOm4uNLfCP-l3tygCtgQ_jNd1Rnr2ZZnnRaFR9AP6WysbbMI0fFvKNQRuCZLD78VXQMEAsk-proj-wcIqBozFCPi7FClE3YbShy5Uf-eQIAOqsi19_6tmXwY1lgJzd8r-4DqY6NSXatSd2Ets2cpJ0VT3BlbkFJ8r__1WOm4uNLfCP-l3tygCtgQ_jNd1Rnr2ZZnnRaFR9AP6WysbbMI0fFvKNQRuCZLD78VXQMEA
       },
       body: JSON.stringify({
         prompt: message
