513976e48d141bc707a2b069c2bb4b26dc5370f5
Varunkohli018 <varunkohli18h@gmail.com>
2025-07-07 10:25:05 +0530

    Update script.js

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ async function getBotReply(message) {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
-        "Authorization": "Bearer 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"
+        "Authorization": "Bearer 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.
       },
       body: JSON.stringify({
         model: "gpt-3.5-turbo",
